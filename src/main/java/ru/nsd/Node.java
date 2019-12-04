c6ecf453cfbd50d27592cb424fe5849246de8391
Mansur <hwimahw@gmail.com>
2019-12-04 16:49:44 +0300

    Modify building LifaPlan, add more adapting input from file

---
+++ src/main/java/ru/nsd/Node.java
@@ -0,0 +1,13 @@
+package ru.nsd;
+import java.util.ArrayList;
+
+public class Node {
+    private String name;
+
+    private ArrayList<Node> children = new ArrayList<>();
+     Node(){
+
+     }
+
+
+}
