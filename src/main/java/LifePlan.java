b8bd5da27433e4e1815e3f3510c80bfe88efd591
Mansur <hwimahw@gmail.com>
2019-12-12 13:29:47 +0300

    some version of writeToFileDayPlan()

--- src/main/java/LifePlan.java
+++ src/main/java/LifePlan.java
@@ -4,7 +4,6 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 
 import ru.nsd.Node;
--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,6 +1,6 @@
 import ru.nsd.Node;
 
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Manager {
@@ -11,8 +11,55 @@ public class Manager {
     Map<String, String> subjectAndPlan;
 
 
+    private void writeToFileDayPlan() throws Exception{
+        File file = new File("./src/main/resources/input.txt");
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try{
+            fileWriter = new FileWriter(file);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            write(lifePlan.getRoot(), bufferedWriter, "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                bufferedWriter.close();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-    public void select() {
+
+    }
+
+    private void write(Node root, BufferedWriter bufferedWriter, String gaps) throws Exception{
+        if(!isNodeInLeaveArrayList(root.getName())) {
+            bufferedWriter.write(gaps + root.getName());
+        }else {
+            if (root.getPlan() != null) {
+                bufferedWriter.write(gaps + root.getName() + "\n");
+                bufferedWriter.write(gaps + "  " + root.getPlan() + "\n");
+            }
+        }
+        ArrayList<Node> arrayList = root.getChildren();
+        for(int i = 0; i < arrayList.size(); i++){
+            write(arrayList.get(i), bufferedWriter, gaps + "  "+ "\n");
+        }
+    }
+
+    private boolean isNodeInLeaveArrayList(String name){
+        ArrayList<Node> leaves = lifePlan.getLeaves();
+        for(int i = 0; i < leaves.size(); i++){
+            if(leaves.get(i).getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void select() throws Exception{
         int exit = 0;
         do {
             menu.buildPrintMenu();
@@ -39,29 +86,30 @@ public class Manager {
                             if (stringSubjectPlan.length() == 1) {
                                 break;
                             }
-                            stringSubjectPlan.replace("/", "");
+                            stringSubjectPlan = stringSubjectPlan.replace("/", "");
                         }
                         String[] split = stringSubjectPlan.split(":");
                         for(int i = 0; i < split.length; i++){
                             System.out.println(split[i]);
                         }
                         subjectAndPlan.put(split[0], split[1]);
-                        subjectAndPlan = new HashMap<>();
+                  //      subjectAndPlan = new HashMap<>();
                         if (flag == 1) {
                             break;
                         }
                     }
-                    System.out.println(12121);
+//                    for(Map.Entry<String, String> entry: subjectAndPlan.entrySet()){
+//                        System.out.println(entry.getKey() + " " + entry.getValue());
+//                    }
                     DayPlan dayPlan = new DayPlan(date, subjectAndPlan);
                     lifePlan.setPlanInLeavesFromDayPlan(subjectAndPlan);
                     ArrayList<Node> arrayList = lifePlan.getLeaves();
-                    System.out.println(arrayList.size());
                     for(int i = 0; i < arrayList.size(); i++){
-                        System.out.println(3333);
                         Node nod = arrayList.get(i);
                         if(nod.getPlan() != null)
                             System.out.println(nod.getPlan());
                     }
+                    writeToFileDayPlan();
                     break;
                 }
                 case (3): {
